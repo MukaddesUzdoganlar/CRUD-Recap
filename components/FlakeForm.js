2bc2ec81f1bcec0535e73126def3604f8bac7625
robin schubert <robschu123@hotmail.de>
2023-10-06 16:20:11 +0200

    change some stuff

--- components/FlakeForm.js
+++ components/FlakeForm.js
@@ -1,55 +1,30 @@
-export default function FlakeForm({ onSubmit, formName, defaultData }) {
+import { createNewFlake } from "@/lib/api";
+
+export default function FlakeForm({ formName }) {
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const flakes = Object.fromEntries(formData);
-    onSubmit(flakes);
+    createNewFlake(flakes);
   }
   return (
-    <FormContainer aria-labelledby={formName} onsubmit={handleSubmit}>
-      <Label htmlFor="name">Name</Label>
-      <Input
-        type="text"
-        id="name"
-        name="name"
-        defaultValue={defaultData?.name}
-      />
+    <form aria-labelledby={formName} onsubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" />
 
-      <Label htmlFor="brand">Brand</Label>
-      <Input
-        type="text"
-        id="brand"
-        brand="name"
-        defaultValue={defaultData?.brand}
-      />
+      <label htmlFor="brand">Brand</label>
+      <input type="text" id="brand" brand="name" />
 
-      <Label htmlFor="ingredients">Ingredients</Label>
-      <Input
-        type="text"
-        id="ingredients"
-        ingredients="name"
-        defaultValue={defaultData?.ingredients}
-      />
+      <label htmlFor="ingredients">Ingredients</label>
+      <input type="text" id="ingredients" ingredients="name" />
 
-      <Label htmlFor="nutritional_table">Nutritionals</Label>
-      <Input
-        type="text"
-        id="nutritional_table"
-        nutritional_table="name"
-        defaultValue={defaultData?.nutritional_table}
-      />
+      <label htmlFor="nutritional_table">Nutritionals</label>
+      <input type="text" id="nutritional_table" nutritional_table="name" />
 
-      <Label htmlFor="image_url">Image</Label>
-      <Input
-        type="text"
-        id="image_url"
-        image="image_url"
-        defaultValue={defaultData?.image_url}
-      />
+      <label htmlFor="image_url">Image</label>
+      <input type="text" id="image_url" image="image_url" />
 
-      <button type="submit">
-        {defaultData ? "Update flake" : "Add a new flake"}
-      </button>
-    </FormContainer>
+      <button type="submit">Add a new flake</button>
+    </form>
   );
 }
