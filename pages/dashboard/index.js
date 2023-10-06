9b6cfcc60cb26f0ec2be79778cb079175a5f7af6
Felix Sadowski <sadowski.felix@gmail.com>
2023-10-06 16:40:59 +0200

    connected dashboard with form

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,12 @@
 import FlakeTable from "@/components/FlakeTable";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <>
       <h1>Dashboard</h1>
       <FlakeTable />
+      <Link href={"/dashboard/flakes/new"}> Add new Flakes </Link>
     </>
   );
 }
